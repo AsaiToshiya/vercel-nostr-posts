91bbc4a0383f72f42d9c21b524ab1d6b04bf909e
Asai Toshiya <to.asai.60@gmail.com>
2023-02-15 22:25:42 +0900

    最初のバージョン

--- README.md
+++ README.md
@@ -1,6 +1,67 @@
-# node-boilerplate
+# vercel-nostr-posts
+
+## セットアップ
+
+### リポジトリ
 
 ```bash
-sed -i 's/node-boilerplate/<repo>/g' *
-echo "# <repo>" > README.md
+git clone https://github.com/AsaiToshiya/vercel-nostr-posts.git
+cd vercel-nostr-posts
+npm install
+```
+
+### Vercel
+
+#### ログイン
+
+```bash
+$ node ./node_modules/vercel/dist/index.js login to.asai.60@gmail.com
+Vercel CLI 28.15.4
+We sent an email to to.asai.60@gmail.com. Please follow the steps provided inside it and make sure the security code matches Snowy Vampire Bat.
+> Success! Email authentication complete for to.asai.60@gmail.com
+Congratulations! You are now logged in. In order to deploy something, run `vercel`.
+💡  Connect your Git Repositories to deploy every branch push automatically (https://vercel.link/git).
+```
+
+#### プロジェクトの作成
+
+```bash
+$ node ./node_modules/vercel/dist/index.js project add vercel-nostr-posts
+Vercel CLI 28.15.4
+> Success! Project vercel-nostr-posts added (asaitoshiya) [564ms]
+```
+
+#### リンク
+
+```bash
+$ node ./node_modules/vercel/dist/index.js link
+Vercel CLI 28.15.4
+? Set up “~/bin/vercel-nostr-posts”? [Y/n] y
+? Which scope should contain your project? asaitoshiya
+? Found project “asaitoshiya/vercel-nostr-posts”. Link to it? [Y/n] y
+✅  Linked to asaitoshiya/vercel-nostr-posts (created .vercel and added it to .gitignore)
+```
+
+### crontab
+
+```bash
+0 * * * * cd /home/pi/bin/vercel-nostr-posts && npm run deploy > /dev/null 2>&1
+```
+
+## npm スクリプト
+
+```
+$ npm run
+Lifecycle scripts included in vercel-nostr-posts@1.0.0:
+  test
+    echo "Error: no test specified" && exit 1
+
+available via `npm run-script`:
+  prebuild
+    rm -rf public && mkdir public
+  build
+    node index.js && mv -f index.html ./public
+  deploy
+    npm run build && vercel --prod
+
 ```
--- index.js
+++ index.js
@@ -1,1 +1,81 @@
-#!/usr/bin/env node
+import * as fs from "fs";
+
+import pkg from "nostr-tools";
+const { SimplePool } = pkg;
+import "websocket-polyfill";
+
+// 投稿者の公開鍵 (16 進数)
+const PK = "0a2f19dc1a185792c3b0376f1d7f9971295e8932966c397935a5dddd1451a25a";
+
+// リレー サーバー
+const RELAYS = [
+  "wss://nos.lol",
+  "wss://nostr-pub.wellorder.net",
+  "wss://nostr.bitcoiner.social",
+  "wss://nostr.mom",
+  "wss://nostr.oxtr.dev",
+  "wss://relay.damus.io",
+  "wss://relay.nostr.bg",
+];
+
+// 投稿を取得する
+const pool = new SimplePool();
+const posts = await pool.list(RELAYS, [
+  {
+    authors: [PK],
+    kinds: [1],
+  },
+]);
+
+// 日時の降順にソートして、日ごとにグループ化する
+const sortedPosts = [...posts].sort((a, b) => b.created_at - a.created_at);
+const groupedPosts = sortedPosts.reduce((acc, obj) => {
+  const date = new Date(obj.created_at * 1000);
+  const key = date.toLocaleDateString();
+  const curGroup = acc[key] ?? [];
+  return { ...acc, [key]: [...curGroup, obj] };
+}, {});
+
+// HTML を作成する
+const html =
+  `<!DOCTYPE html>
+  <html lang="ja">
+    <head>
+      <meta charset="utf8" />
+      <title>メモ</title>
+    </head>
+    <body>
+      <h1>メモ</h1>
+` +
+  Object.keys(groupedPosts)
+    .map(
+      (postDay) =>
+        `      <h2>${postDay}</h2>
+` +
+        groupedPosts[postDay]
+          .map((post) => {
+            const date = new Date(post.created_at * 1000);
+            const time = date.toLocaleTimeString();
+            const content = post.content
+              .replace(/&/g, "&amp;")
+              .replace(/</g, "&lt;")
+              .replace(/>/g, "&gt;")
+              .replace(/"/g, "&quot;")
+              .replace(/'/g, "&#039;")
+              .replace(/ /g, "&nbsp;")
+              .replace(/\n/g, "<br />");
+            return `      <h3>${time}</h3>
+      <p>${content}</p>`;
+          })
+          .join("\n")
+    )
+    .join("\n") +
+  `
+    </body>
+  </html>`;
+
+// ファイルに出力する
+fs.writeFileSync("index.html", html);
+
+// await pool.close(RELAYS); // TypeError: Cannot read properties of undefined (reading 'sendCloseFrame')
+process.exit(); // HACK: 強制終了する
