81e5f268d07462ac040f6ea8648b3f325fdaf29d
Asai Toshiya <to.asai.60@gmail.com>
2023-02-21 01:27:18 +0900

    タブをスペースに変換する

--- index.js
+++ index.js
@@ -44,19 +44,19 @@ const html =
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="github-markdown.css">
       <style>
-      	.markdown-body {
-      		box-sizing: border-box;
-      		min-width: 200px;
-      		max-width: 980px;
-      		margin: 0 auto;
-      		padding: 45px;
-      	}
+        .markdown-body {
+          box-sizing: border-box;
+          min-width: 200px;
+          max-width: 980px;
+          margin: 0 auto;
+          padding: 45px;
+        }
       
-      	@media (max-width: 767px) {
-      		.markdown-body {
-      			padding: 15px;
-      		}
-      	}
+        @media (max-width: 767px) {
+          .markdown-body {
+            padding: 15px;
+          }
+        }
       </style>
       <title>メモ</title>
     </head>
