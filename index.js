cb7e8788148af8b5df33b140005fecad969a1582
Asai Toshiya <to.asai.60@gmail.com>
2023-03-07 00:27:43 +0900

    ハッシュタグ ページを出力するようにする
    Closes #2

--- index.js
+++ index.js
@@ -85,5 +85,72 @@ const html =
 // ファイルに出力する
 fs.writeFileSync("index.html", html);
 
+const groupedPosts2 = sortedPosts.reduce((acc1, obj1) => {
+  const tags =
+    obj1.content.match(
+      /(^|\s)#[a-z0-9\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+/gi
+    ) ?? [];
+  return tags.reduce((acc2, obj2) => {
+    const tag = obj2.trim();
+    const key =
+      Object.keys(acc2).find((key) => key.toLowerCase() == tag.toLowerCase()) ??
+      tag;
+    const curGroup = acc2[key] ?? [];
+    return { ...acc2, [key]: [...curGroup, obj1] };
+  }, acc1);
+}, {});
+
+// HTML を作成する
+const html2 =
+  `<!DOCTYPE html>
+  <html lang="ja">
+    <head>
+      <meta charset="utf8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="github-markdown.css">
+      <style>
+        .markdown-body {
+          box-sizing: border-box;
+          min-width: 200px;
+          max-width: 980px;
+          margin: 0 auto;
+          padding: 45px;
+        }
+      
+        @media (max-width: 767px) {
+          .markdown-body {
+            padding: 15px;
+          }
+        }
+      </style>
+      <title>メモ</title>
+    </head>
+    <body class="markdown-body">
+      <h1>メモ</h1>
+` +
+  Object.keys(groupedPosts2)
+    .sort((a, b) => (a.toLowerCase() < b.toLowerCase() ? -1 : 1))
+    .map(
+      (tag) =>
+        `      <h2>${tag}</h2>
+` +
+        groupedPosts2[tag]
+          .map((post) => {
+            const date = new Date(post.created_at * 1000);
+            const dateTime = date.toLocaleString();
+            const content = marked.parse(post.content);
+            return `      <h3>${dateTime}</h3>
+      <p>${content}</p>`;
+          })
+          .join("\n")
+    )
+    .join("\n") +
+  `
+    </body>
+  </html>`;
+
+// ファイルに出力する
+fs.writeFileSync("hashtag.html", html2);
+
 // await pool.close(RELAYS); // TypeError: Cannot read properties of undefined (reading 'sendCloseFrame')
 process.exit(); // HACK: 強制終了する
